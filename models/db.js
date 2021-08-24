dc7eeb9c4b8724ac84d33043966d0a4c1a2e22ff
PRADEEPT07 <55102807+PRADEEPT07@users.noreply.github.com>
2021-08-24 20:08:01 +0530

    Main

---
+++ models/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/EmployeeDB',{ useNewUrlParser : true},(err)=>{
+    if(!err) {
+        console.log('MongoDB connection Succeeded')
+    }
+    else {
+        console.log('Erroe in DB connection :' + err)
+    }  
+})
+
+require('./employee.model')
